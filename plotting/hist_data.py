c05a29a81fd87f4a2b8bff38c26416be1a271d0f
johanngan <johanngan.us@gmail.com>
2019-05-28 15:07:09 -0400

    Change to generic test input file

--- plotting/hist_data.py
+++ plotting/hist_data.py
@@ -6,7 +6,7 @@ Generate a 1D histogram from data
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = '/Users/johanngan/Desktop/research/purdue/berylliumplus/vel.txt'
+fname = 'test.txt'
 data = np.loadtxt(fname, skiprows=0, usecols=0)
 plt.hist(data, bins=100, density=True)
 plt.show()
