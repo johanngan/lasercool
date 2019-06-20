a7dd2aa57fb8aef939842116c8293fd216b27e4d
johanngan <johanngan.us@gmail.com>
2019-06-20 14:32:55 -0400

    Add surface plot script

---
+++ scripts/plotting/surf_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Generate a surface plot from data in tall ordered format.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+######## CONFIGURATION ########
+# numpy.loadtxt() stuff
+fname = 'output/*/*.out'
+skiprows = 0
+usecols = None  # Can only use three columns at a time
+######## END CONFIGURATION ########
+
+data = np.loadtxt(fname, skiprows=skiprows, usecols=usecols)
+
+# Convert from tall format to meshgrid format
+X, Y = np.meshgrid(np.unique(data[:, 0]), np.unique(data[:, 1]))
+Z = np.reshape(data[:, 2], X.shape, order='F')
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, cmap=cm.jet)
+fig.colorbar(surf)
+
+plt.show()
