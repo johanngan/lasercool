70fb6c9825f01334c082e07f14b149f4cbf20f85
johanngan <johanngan.us@gmail.com>
2019-06-21 17:00:42 -0400

    Move file output to separate functions

--- src/swapcool/swapmotion.cpp
+++ src/swapcool/swapmotion.cpp
@@ -33,14 +33,6 @@ int main(int argc, char** argv) {
     // d(rho)/d(Gamma*t)
     HMotion hamil(cfg_file);
 
-    std::vector<std::complex<double>> rho_c0(hamil.nmat());
-    // Initialize all in one k-state
-    rho_c0[hamil.subidx(1, init_k, 1, init_k)] = 1;
-
-    // Solve the system in natural units with an adaptive RK method
-    auto rho_c_solution = timestepping::odesolve(hamil, rho_c0,
-        duration_by_decay, timestepping::AdaptiveRK(tol));
-
     // Form output files
     std::ostringstream oftag_ss;
     oftag_ss << std::setprecision(OUTFILENAME_PRECISION)
@@ -72,8 +64,16 @@ int main(int argc, char** argv) {
         OUTPUT_DIR
     ));
 
+    // Solve the system
+    std::vector<std::complex<double>> rho_c0(hamil.nmat());
+    // Initialize all in one k-state
+    rho_c0[hamil.subidx(1, init_k, 1, init_k)] = 1;
+
+    // Solve the system in natural units with an adaptive RK method
+    auto rho_c_solution = timestepping::odesolve(hamil, rho_c0,
+        duration_by_decay, timestepping::AdaptiveRK(tol));
+
     // Write the solution in SI units
-    // Column order is t, |rho11|, |rho22|, |rho33|
     // Write table header
     rho_out << "t |rho11| |rho22| |rho33| tr(rho) tr(rho^2)";
     for(int k = 0; k <= hamil.kmax; ++k) {
@@ -82,45 +82,50 @@ int main(int argc, char** argv) {
     rho_out << " |k_rms|";
     rho_out << std::endl;
     // Write table header
-    kdistout << "t k P(k) P(n = 0, k), P(n = 1, k), P(n = 2, k)" << std::endl;
+    std::string kdist_header = "t k P(k) P(n = 0, k), P(n = 1, k), P(n = 2, k)";
+    kdistout << kdist_header << std::endl;
     for(auto point: rho_c_solution) {
         auto rho = hamil.density_matrix(point.first, point.second);
-        rho_out << point.first / decay_rate // Convert from Gamma*t to just t
-            << " " << std::real(hamil.partialtr_k(rho, 0))
-            << " " << std::real(hamil.partialtr_k(rho, 1))
-            << " " << std::real(hamil.partialtr_k(rho, 2))
-            << " " << std::real(hamil.totaltr(rho))
-            << " " << std::real(hamil.purity(rho));
-        double krms = 0;    // RMS k value
-        for(int k = 0; k <= hamil.kmax; ++k) {
-            auto ktr = hamil.partialtr_n(rho, k);
-            if(k != 0) {
-                ktr += hamil.partialtr_n(rho, -k);
-            }
-            rho_out << " " << std::real(ktr);
-            krms += std::real(ktr) * k*k;
-        }
-        rho_out << " " << sqrt(krms);
-        rho_out << std::endl;
-
-        for(int k = -hamil.kmax; k <= hamil.kmax; ++k) {
-            kdistout << point.first / decay_rate
-                << " " << k << " " << std::real(hamil.partialtr_n(rho, k))
-                << " " << std::real(rho[hamil.subidx(0, k, 0, k)])
-                << " " << std::real(rho[hamil.subidx(1, k, 1, k)])
-                << " " << std::real(rho[hamil.subidx(2, k, 2, k)])
-                << std::endl;
-        }
+        double time = point.first / decay_rate;
+        write_state_info(rho_out, time, rho, hamil);
+        write_kdist(kdistout, time, rho, hamil);
     }
     rho_out.close();
     kdistout.close();
 
     // Output just the final k distribution for convenience
-    kdistfinalout << "k P(k) P(n = 0, k), P(n = 1, k), P(n = 2, k)" << std::endl;
+    kdistfinalout << kdist_header << std::endl;
     auto rho = hamil.density_matrix(
         rho_c_solution.back().first, rho_c_solution.back().second);
+    write_kdist(kdistfinalout, rho_c_solution.back().first / decay_rate,
+        rho, hamil);
+}
+
+void write_state_info(std::ofstream& outfile, double t,
+    const std::vector<std::complex<double>>& rho, const HMotion& hamil) {
+    outfile << t
+        << " " << std::real(hamil.partialtr_k(rho, 0))
+        << " " << std::real(hamil.partialtr_k(rho, 1))
+        << " " << std::real(hamil.partialtr_k(rho, 2))
+        << " " << std::real(hamil.totaltr(rho))
+        << " " << std::real(hamil.purity(rho));
+    double krms = 0;    // RMS k value
+    for(int k = 0; k <= hamil.kmax; ++k) {
+        auto ktr = hamil.partialtr_n(rho, k);
+        if(k != 0) {
+            ktr += hamil.partialtr_n(rho, -k);
+        }
+        outfile << " " << std::real(ktr);
+        krms += std::real(ktr) * k*k;
+    }
+    outfile << " " << sqrt(krms);
+    outfile << std::endl;
+}
+void write_kdist(std::ofstream& outfile, double t,
+    const std::vector<std::complex<double>>& rho, const HMotion& hamil) {
     for(int k = -hamil.kmax; k <= hamil.kmax; ++k) {
-        kdistfinalout << " " << k << " " << std::real(hamil.partialtr_n(rho, k))
+        outfile << t
+            << " " << k << " " << std::real(hamil.partialtr_n(rho, k))
             << " " << std::real(rho[hamil.subidx(0, k, 0, k)])
             << " " << std::real(rho[hamil.subidx(1, k, 1, k)])
             << " " << std::real(rho[hamil.subidx(2, k, 2, k)])
--- src/swapcool/swapmotion.hpp
+++ src/swapcool/swapmotion.hpp
@@ -6,9 +6,17 @@
 #include <string>
 #include <fstream>
 #include <complex>
+#include <vector>
 #include "HMotion.hpp"
 #include "lasercool/readcfg.hpp"
 #include "lasercool/iotag.hpp"
 #include "lasercool/timestepping.hpp"
+
+// Write state info to a file given the density matrix at a fixed time
+void write_state_info(std::ofstream&, double,
+    const std::vector<std::complex<double>>&, const HMotion&);
+// Write the k-distribution at a fixed time to a file in tall format
+void write_kdist(std::ofstream&, double,
+    const std::vector<std::complex<double>>&, const HMotion&);
 
 #endif
